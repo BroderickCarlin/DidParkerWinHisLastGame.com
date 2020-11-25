da384047e6f32b7bb6e13b6e67951ce599f08fea
Broderick Carlin <broderick.carlin@gmail.com>
2020-11-24 18:25:53 -0600

    Add utility functions

--- www/index.js
+++ www/index.js
@@ -1,3 +1,16 @@
-function test() {
-	console.log("hello!");
+async function getPageHtml() {
+	let resp = await fetch("https://didparkerwinhislastgame.netlify.app/opgg");
+	let html = await resp.text();
+	var htmlObject = document.createElement('div');
+	htmlObject.innerHTML = html;
+	return htmlObject;
+}
+
+function getMinutesSinceUpdate(htmlObject) {
+	let lastUpdate = htmlObject.getElementsByClassName("LastUpdate");
+	let timestamp = Number(lastUpdate[0].firstElementChild.dataset.datetime) * 1000;
+	var updatedTimestamp = new Date(timestamp);
+	var now = new Date();
+	let delta = Math.floor((now - updatedTimestamp) / 60000);
+	return delta;
 }
